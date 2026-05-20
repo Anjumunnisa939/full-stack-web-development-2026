13f7214d275cf87cb91bc487b233209d85e84416
Anjumunnisa939 <anjumrpt@gmail.com>
2026-05-20 22:40:15 +0530

    boolean operators

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,25 @@
 alert('hello worlds');
     console.log(2+2);
-    document.body.innerHTML=2+2;
-    
+   
+   
+    let a=5;
+    let b=6;
+    let c=a>b;
+    console.log(c);
+    if(c){
+        console.log('HII DR.ANJUMUNNISA');
+    }
+    else{
+        console.log(`sorry DR.ANJUMUNNISA`);
+    }
+    function checkAge(){
+let age = document.getElementById('age').value;
+console.log(age);
+age=age>18;
+if(age){
+    console.log('your eligible to drive the car and bike');
+}
+else{
+    console.log('your not eligible SORRY!!!');
+}
+    }
